13c258f42d7b3a466145274db5b7b9c172e10f39
suman <suman143learning@gmail.com>
2020-05-14 20:05:17 +0530

    fixed tc 2

--- project11/tests/testsample2.py
+++ project11/tests/testsample2.py
@@ -2,6 +2,7 @@ class A:
     def m1(self):
         print("m1")
     def m2(self):
-        print("m2")
+        print("m23")
     def m3(self):
-        print("m3")
+        print("m31")
+        print("m32")
